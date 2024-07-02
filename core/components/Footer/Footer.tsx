e049183309102e9af0af85f0ae75601d787c653c
frankzhuao123 <91228378+frankzhuao123@users.noreply.github.com>
2024-07-01 20:28:28 -0400

    Added Footer

--- core/components/Footer/Footer.tsx
+++ core/components/Footer/Footer.tsx
@@ -1,6 +1,13 @@
-import { styled, Grid, Text, EM, Flex } from '@maximeheckel/design-system';
-// import Logo from '@core/components/Logo';
-// import Link from 'next/link';
+import {
+  styled,
+  Anchor,
+  Grid,
+  Text,
+  EM,
+  Flex,
+} from '@maximeheckel/design-system';
+import Logo from '@core/components/Logo';
+import Link from 'next/link';
 import React from 'react';
 import { templateColumnsMedium } from 'styles/grid';
 
@@ -17,15 +24,60 @@ const FooterContent = styled(Flex, {
 });
 
 // TODO: Make more modular
+
+
 const Footer = () => (
   <FooterBlock data-testid="footer">
     <Grid gapX={4} templateColumns={templateColumnsMedium}>
-      <FooterContent alignItems="center" justifyContent="space-between">
-        <Text as="p" css={{ margin: 0 }} size="1" variant="primary" weight="3">
-          © {new Date().getFullYear()} Frank Zhu —— <EM size="1">New York</EM>
-        </Text>
-        {/* <Logo alt="Maxime Heckel's logo" size={35} /> */}
-      </FooterContent>
+      <Flex
+        as={Grid.Item}
+        col={2}
+        css={{
+          padding: 'var(--space-5) 0px',
+        }}
+        direction="column"
+        justifyContent="space-evenly"
+        gap={6}
+      >
+        <Grid css={{ width: '100%' }} templateColumns="repeat(3, 1fr)">
+          <Grid.Item>
+            <Text size={1}>
+              <Grid>
+                <Link href="/" legacyBehavior passHref>
+                  <Anchor discreet>Home</Anchor>
+                </Link>
+              </Grid>
+            </Text>
+          </Grid.Item>
+          <Grid.Item>
+            <Text size={1}>
+              <Grid>
+                <Anchor
+                  discreet
+                  href="https://github.com/TDR474"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Github
+                </Anchor>
+              </Grid>
+            </Text>
+          </Grid.Item>
+        </Grid>
+        <FooterContent alignItems="center" justifyContent="space-between">
+          <Text
+            as="p"
+            css={{ margin: 0 }}
+            size="1"
+            variant="primary"
+            weight="3"
+          >
+            © {new Date().getFullYear()} Frank Zhu ——{' '}
+            <EM size="1">New York</EM>
+          </Text>
+          <Logo alt="Frank's logo" size={35} />
+        </FooterContent>
+      </Flex>
     </Grid>
   </FooterBlock>
 );
